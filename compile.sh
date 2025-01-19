c084b474ad00b568783bddcf5ec7833117b473e0
Robert Roland <rob@retronauts.org>
2025-01-18 16:11:42 -0800

    Cleanup

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pushd out
-cobc -I../temp/ -x ../temp/simple.cbl ../temp/fcgi.cbl -o simple.fcgi -A '-include fcgi_config.h' -A '-include fcgi_stdio.h' -lfcgi --debug -g
-retval=$?
-popd
-
-exit $retval
